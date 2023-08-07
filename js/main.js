7fe02dceec4db3b866c1f0a7c6797fac131a2130
KurtSchwimmbacher <231002@virtualwindow.co.za>
2023-08-07 23:07:00 +0200

    add sections to home page and trips page
    add outline for accordion and footer, add beginning of trips page, begin outlining trips array in main.js

---
+++ js/main.js
@@ -0,0 +1,30 @@
+// ===============================================================
+// variables
+const tripArr = [
+    {
+        name : "",
+        destinations: "",
+        duration: "",
+        departurePort: "",
+        price: "",
+        roundTrip: true
+    },
+    {
+        name : "",
+        destinations: "",
+        duration: "",
+        departurePort: "",
+        price: "",
+        roundTrip: true
+    }    
+];
+
+    let appliedFilter = "";
+    let appliedSort = "date added";
+
+
+
+
+$(document).ready(function (){
+    console.log("Hello")
+});
