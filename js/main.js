e55de9ce9e139ce65e85fd14c44da50aeccfcff7
KurtSchwimmbacher <231002@virtualwindow.co.za>
2023-08-08 11:03:37 +0200

    add all images and array
    add all images for the trips and array for trips

--- js/main.js
+++ js/main.js
@@ -2,27 +2,70 @@
 // variables
 const tripArr = [
     {
-        name : "",
-        destinations: "",
-        duration: "",
-        departurePort: "",
-        price: "",
+        name : "South East Asian Beach Cruise",
+        picture: "../assets/trips/Ko Phi Phi of Thailand.jpg",
+        destinations: "Thailand, Maldives, Seychelles",
+        duration: "14 Days",
+        departurePort: "Port Louis, Mauritius",
+        price: "$3 534",
         roundTrip: true
     },
     {
-        name : "",
-        destinations: "",
-        duration: "",
-        departurePort: "",
-        price: "",
+        name : "Arctic Peninsula Cruise ",
+        picture: "../assets/trips/seal explore.png",
+        destinations: "Greenland, Iceland, Faroe Islands",
+        duration: "15 Days",
+        departurePort: "Oslo, Norway",
+        price: "$5 847",
         roundTrip: true
-    }    
+    },
+    {
+        name : "Caribbean Islands Cruise",
+        picture: "../assets/trips/carribean.jpg",
+        destinations: "Cayman Islands, Barbados, Aruba",
+        duration: "11 Days",
+        departurePort: "Havana, Cuba",
+        price: "$3 134",
+        roundTrip: true
+    },     
+    {
+        name : "African Cruise",
+        picture: "../assets/trips/Africa Trip.png",
+        destinations: "Namibia, Cape Verde, Angola ",
+        duration: "13 Days",
+        departurePort: "Cape Town, South Africa",
+        price: "$4 934",
+        roundTrip: true
+    },
+    {
+        name : "Mediterranean Cruise",
+        picture: "../assets/trips/Greece Trip.png",
+        destinations: "Italy, Greece, Turkey",
+        duration: "9 Days",
+        departurePort: "Tel-Aviv, Isreal",
+        price: "$1 834",
+        roundTrip: true
+    },
+    {
+        name : "Alaskan Cruise",
+        picture: "../assets/trips/Alaska Trip.png",
+        destinations: "Hubbard Glacier, Skagway, Juneau",
+        duration: "9 Days",
+        departurePort: "Oslo, Norway",
+        price: "$7 789",
+        roundTrip: true
+    }
 ];
 
     let appliedFilter = "";
     let appliedSort = "date added";
 
 
+
+
+
+// ==================================================================================================================================
+// load all trips
 
 
 $(document).ready(function (){
